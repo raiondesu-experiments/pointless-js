e4633f9facb39bb1aaac42627fef151b219c82ec
Raiondesu <a.raiondesu@gmail.com>
2019-11-13 17:33:16 +0300

    Add [mapObject] src/object

--- src/object.ts
+++ src/object.ts
@@ -9,6 +9,21 @@ import { Nullable } from './type';
 export const safeObj = createSafeOperation<any>({} as any);
 
 /**
+ * Maps object values by their keys (including symbols) into a new object
+ *
+ * Generaly equivalent to `Array.prototype.map()`
+ */
+export const mapObject = <T extends object, R>(
+  obj: T,
+  value: (key: keyof T) => R
+) => (
+  <Array<keyof T>>
+  [...Object.keys(obj), ...Object.getOwnPropertySymbols(obj)]
+).reduce((newObj, key) => (
+  newObj[key] = value(key), newObj
+), <Record<keyof T, R>> {});
+
+/**
  * Gets the array of keys of the given object (type-safe)
  */
 export const keys = Object.keys.bind(Object) as <T extends object>(o: T) => Array<keyof T>;
